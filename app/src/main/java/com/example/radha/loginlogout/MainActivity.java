9abdc2b3fafc44e8399baeed058c97f6ecaf17bb
radha-panda <radha.panda1094@gmail.com>
2018-03-11 16:02:09 +0530

    This is my first commit

---
+++ app/src/main/java/com/example/radha/loginlogout/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.radha.loginlogout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnLogout;
+    private Session session;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        session = new Session(this);
+        if (!session.loggedin()) {
+            logout();
+        }
+        btnLogout = findViewById(R.id.btnLogout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout();
+            }
+        });
+    }
+
+    private void logout() {
+        session.setLoggedin(false);
+        finish();
+        startActivity(new Intent(MainActivity.this, loginactivity.class));
+    }
+}

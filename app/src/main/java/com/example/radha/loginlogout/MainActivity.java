ec70e0bf92f2b8736d13a8eb97b06182f6c4a4a3
radha-panda <radha.panda1094@gmail.com>
2018-03-12 17:04:37 +0530

    This is my first commit

--- app/src/main/java/com/example/radha/loginlogout/MainActivity.java
+++ app/src/main/java/com/example/radha/loginlogout/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.radha.loginlogout;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private Button btnLogout;
@@ -14,6 +19,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.lo_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        TextAdapter textAdapter = new TextAdapter();
+        recyclerView.setAdapter(textAdapter);
+
+
+        List<String> stringList = new ArrayList<>();
+        stringList.add("cakes");
+        stringList.add("chocolates");
+        stringList.add("oranges");
+        stringList.add("strawberry");
+        stringList.add("blackcurrent");
+        stringList.add("rasaberry");
+        stringList.add("android");
+
+        List<String> list = new ArrayList<>();
+        list.addAll(stringList);
+        list.addAll(stringList);
+        list.addAll(stringList);
+        list.addAll(stringList);
+
+
+        textAdapter.setItems(list);
+
 
         session = new Session(this);
         if (!session.loggedin()) {
